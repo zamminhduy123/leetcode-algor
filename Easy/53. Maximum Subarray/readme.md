113472d432f6c4ef443faf01c62e67df655e87f5
zamminhduy123 <ntminhduy123@gmail.com>
2022-04-26 22:25:08 +0700

    2 way Maximum SubArray

---
+++ Easy/53. Maximum Subarray/readme.md
@@ -0,0 +1,7 @@
+## Thought Process
+
+2 Pointers solution can very easily see in this situation. In fact, it's a common use of 2 pointers
+
+## Follow up
+
+Follow up question ask to solve this using divide and conquer strategy which called Kadane - a common one as well.
--- Easy/53. Maximum Subarray/solution_2Way.js
+++ Easy/53. Maximum Subarray/solution_2Way.js
@@ -2,6 +2,8 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+// 2 pointers solution
 var maxSubArray = function(nums) {
     let left = 0, right = 0;
     let sum = 0, max = nums[0];
@@ -18,6 +20,24 @@ var maxSubArray = function(nums) {
             sum -= nums[left];
             left++;
         }
+    }
+    return max;
+};
+
+//Kadane: Divided and Conquer Solution
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let newNums = [];
+    let max = nums[0];
+    newNums.push(nums[0])
+    for (let i= 1; i < nums.length; i++){
+        let temp = nums[i] + newNums[i-1];
+        newNums[i] = temp < nums[i] ? nums[i] : temp;
+        if (newNums[i] > max)
+            max = newNums[i];
     }
     return max;
 };
